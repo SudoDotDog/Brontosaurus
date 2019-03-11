33a2fc347a098cecc061197afa5c728861319835
WMXPY <wm@mengw.io>
2019-03-11 11:05:18 -0500

    add pull install function

--- build/common/clone.py
+++ build/common/clone.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin python3
 # -*- coding: utf-8 -*-
 
 import os
@@ -7,14 +7,25 @@ import subprocess
 BASE_URL = os.path.abspath(os.path.join(
     __file__, os.pardir, os.pardir, os.pardir))
 
-print(BASE_URL)
-
-def cloneServer(origination, repository, target):
+def cloneRepository(origination, repository, target):
     origin = "https://github.com/{0}/{1}".format(origination, repository)
     child = subprocess.Popen(["git", "clone", origin, target], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     child.wait()
     rc = child.returncode
-    print(rc)
-    
+    return rc == 0
 
-cloneServer("SudoDotDog", "Brontosaurus", os.path.join(BASE_URL, 'module', 'portal'))
+def pullRepository(target):
+    child = subprocess.Popen(["git", "pull"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=target)
+    child.wait()
+    rc = child.returncode
+    return rc == 0
+
+def installPackage(target):
+    child = subprocess.Popen(["make", "install"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=target)
+    child.wait()
+    rc = child.returncode
+    return rc == 0
+
+print(cloneRepository("SudoDotDog", "Brontosaurus", os.path.join(BASE_URL, 'module', 'portal')))
+print(pullRepository(os.path.join(BASE_URL, 'module', 'portal')))
+print(installPackage(os.path.join(BASE_URL, 'module', 'portal')))
