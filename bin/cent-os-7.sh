a05dbb81afad4b959b57cd0ece74e328f7b38b7c
WMXPY <wm@mengw.io>
2019-05-17 19:29:15 -0500

    add centos 7 script

---
+++ bin/cent-os-7.sh
@@ -0,0 +1,19 @@
+# Dependencies install on cent os 7
+
+sudo yum remove \
+    docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-selinux \
+    docker-engine-selinux \
+    docker-engine
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum -y install docker-ce
+sudo systemctl start docker
